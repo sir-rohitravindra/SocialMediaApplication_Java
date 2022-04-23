8ba9a962e449ee992ff7f8a7341bca3ff4e75302
Rohit <rohit.ravindra007@gmail.com>
2022-04-23 23:30:10 +0530

    completed routing route including logout

--- PostsPage.java
+++ PostsPage.java
@@ -5,8 +5,16 @@ import java.awt.event.*;
 public class PostsPage extends Page {
 
     private JMenuBar menuBar;
+
     private JMenu accountMenu;
+    private JMenuItem profileItem;
+    private JMenuItem logoutItem;
+
     private JMenu postContentMenu;
+    private JMenu postNewItem;
+    private JMenu newImagePostItem;
+    private JMenu newTextPostItem;
+
     private User curUser;
 
     public PostsPage(int w, int h, String headString, Status defStatus) {
@@ -14,9 +22,22 @@ public class PostsPage extends Page {
         menuBar = new JMenuBar();
         this.setJMenuBar(menuBar);
 
-        postContentMenu = new JMenu("Create");
+        accountMenu = new JMenu("User: ");
+        profileItem = new JMenuItem("View Profile");
+        logoutItem = new JMenuItem("Logout");
+        accountMenu.add(profileItem);
+        accountMenu.add(logoutItem);
 
-        // menuBar.add(accountMenu);
+        postContentMenu = new JMenu("Create");
+        postNewItem = new JMenu("New Post");
+        postContentMenu.add(postNewItem);
+
+        newImagePostItem = new JMenu("New Image Post");
+        newTextPostItem = new JMenu("New Text Post");
+        postNewItem.add(newImagePostItem);
+        postNewItem.add(newTextPostItem);
+
+        menuBar.add(accountMenu);
         menuBar.add(postContentMenu);
 
     }
@@ -24,12 +45,18 @@ public class PostsPage extends Page {
     public void setCurUser(User curUser) {
 
         this.curUser = curUser;
-        accountMenu = new JMenu("User: " + curUser.getName());
-        menuBar.add(accountMenu);
+        // accountMenu = new JMenu("User: " + curUser.getName());
+        // menuBar.add(accountMenu);
+        accountMenu.setText("User:" + curUser.getName());
         menuBar.revalidate();
-        // accountMenu.setText(curUser.getName());
-        System.out.println(this.curUser);
-        System.out.println(accountMenu.getText());
+
+        // System.out.println(this.curUser);
+        // System.out.println(accountMenu.getText());
+    }
+
+    public void AddLogoutListener(ActionListener logoutListener) {
+        logoutItem.setActionCommand("Logout");
+        logoutItem.addActionListener(logoutListener);
     }
 
 }
